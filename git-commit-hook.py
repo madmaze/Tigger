671915b8ef1f0c24bffb1bee831991cd28e40ab9
Matthias Lee <MatthiasLee@jhu.edu>
2012-06-18 14:32:09 -0400

    started work on json commit hook

--- git-commit-hook.py
+++ git-commit-hook.py
@@ -2,8 +2,14 @@
 
 import os
 import datetime
+import json
 from subprocess import call
 commits=[]
+fields=["tid","task","createdOn","createdBy","state","priority"]
+
+# Return the current git user
+def getGitUser():
+	return "Matthias Lee"
 
 def main():
 	# Grabs the latest commit and then sees if there is a matching task
@@ -12,21 +18,55 @@ def main():
 	liveTasks = open(".tigger","w")
 	completedTasks = open(".tigger_completed","a")
 	for t in tasks:
-		if lastCommit["msg"].lower().find(t.lower()) >= 0:
+		print t['task'].lower()
+		if lastCommit["msg"].lower().find(t['task'].lower()) >= 0:
 			print "completed:",t,datetime.datetime.now()
-			completedTasks.write('{"completed":"'+str(datetime.datetime.now())+'","task":"'+t+'"}\n')
+			print '{"completed":"'+str(datetime.datetime.now())+'","task":"'+t['task']+'"}\n'
+			t["closed"]=str(datetime.datetime.now())
+			t["closedBy"]=getGitUser()
+			t["state"]="completed"
+			#completedTasks.write('{"completed":"'+str(datetime.datetime.now())+'","task":"'+t+'"}\n')
+			print json.dumps(t)
+			liveTasks.write(json.dumps(t)+"\n")
 		else:
-			liveTasks.write(t+"\n")
+			liveTasks.write(json.dumps(t)+"\n")
 			
 	liveTasks.close()
 	completedTasks.close()
+
+# Parse and Validate JSON
+def parseJson(t_json):
+	missingFields=""
+	if t_json.strip() != "":
+		try:
+			# Parse Json, the verify
+			jsonBits = json.loads(t_json.strip())
+			
+			valid=True
+			# Check whether all required fields are present
+			for f in fields:
+				if f not in jsonBits:
+					valid=False
+					missingFields+=f+" "
+			if valid:
+				return jsonBits
+			else:
+				print "Missing JSON fields:",missingFields
+		except:
+			print "Error: invalid json entry. Skipping:",t_json.strip()
+	else:
+		# Something went wrong returning blank
+		return ""
 
 def getTasks():
 	taskFile = open(".tigger", "r")
 	tasks=[]
 	for task in taskFile.readlines():
-		if task.strip()!="":
-			tasks.append(task.strip())
+		jsonBits = parseJson(task)
+		if jsonBits != "" and jsonBits != None:
+			print "here:",type(jsonBits),jsonBits
+			tasks.append(jsonBits)
+			
 	return tasks
 
 # Returns all git commits in a List of Dicts, 1 for each commit
